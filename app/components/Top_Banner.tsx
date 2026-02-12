d16907ecf65aaefc82183a175729bb04a4488b76
angele <angele.denys@telecomnancy.eu>
2026-02-12 09:59:34 +0100

    feat #5: add Top_Banner files

---
+++ app/components/Top_Banner.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import styles from "./Top_Banner.module.scss";
+import { AuthButton } from "./AuthButton";
+
+export const Top_Banner = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+    <header className={styles.topBanner}>
+
+        <button className={styles.menuToggle} onClick={toggleMenu}>
+            {isOpen ? "MENU ☰" : "MENU ☰"}
+        </button>
+        {isOpen && ( // Conditional rendering of the menu
+            <nav className={styles.mainMenu}> 
+                <ul>
+                    <li className={styles.menuItem}>
+                        <Link href="/sleepLog"> Registre Sommeil </Link> 
+                    </li>
+                    <li className={styles.menuItem}>
+                        <Link href="/medias"> Films/Series/Livres </Link>
+                    </li>
+                </ul>
+            </nav>
+        )}    
+
+        <div className={styles.middle}> Site perso d'Angèle ! </div>
+
+        <div className={styles.rightSection}>
+            <div className={styles.homeButton}> 
+                <Link href="/">HOME</Link> 
+            </div>
+
+            <AuthButton />
+        </div>
+
+    </header>
+    );
+};
+
